591f0ccd0047180590d3be12136e1f997f27812a
Vijay <vijayavc@gmail.com>
2018-02-27 17:31:42 +0000

    Billing summary displayed

--- smart_cart.py
+++ smart_cart.py
@@ -15,6 +15,49 @@ headers = {'Content-type': 'application/json'}
 
 rfid_user_map = { '553128273391': '1' }
 
+def price_to_str(price):
+	price_full = str(price/100)
+	price_dec = str(price%100)
+	if(len(price_dec) ==1):
+		price_dec+='0'
+	return price_full + "." + price_dec
+
+def show_bill_summary(user_id):
+	cart_uri = CART_RESOURCE + cart_id
+	response = requests.get(url = cart_uri)
+	cart_item_data = response.json()
+	print cart_item_data
+	if(len(cart_item_data) > 0):
+		print "------------------------------------------------------------------------------------------------"
+		print "						Your Bill Summary				"
+		print "------------------------------------------------------------------------------------------------"
+		print "   Item					Unit Price		Qty		Price	  "
+		print "------------------------------------------------------------------------------------------------"
+		total_amt = 0
+		for i in range(len(cart_item_data)):
+			cart_item_name = cart_item_data[i]['item']['name']
+			cart_item_unit_price = cart_item_data[i]['item']['price']
+			cart_item_qty = cart_item_data[i]['quantity']
+			cart_item_price = cart_item_unit_price * cart_item_qty
+			total_amt += cart_item_price
+			print "  " + cart_item_name + " 				" + price_to_str(cart_item_unit_price) + "			" + str(cart_item_qty) + "		" + price_to_str(cart_item_price) 
+		print "-------------------------------------------------------------------------------------------------"
+		print " 								Total Amount: " + price_to_str(total_amt)
+		print "-------------------------------------------------------------------------------------------------"
+		print ""
+		print ""
+		print "		Bill Amount: "+ price_to_str(total_amt) + " is detected from your Wallet."
+
+                cart_req = {}
+                cart_req['userId'] = user_id
+                cart_req_js = json.dumps(cart_req)
+		user_uri = USER_RESOURCE+user_id
+		response = requests.get(url = user_uri)
+		user_data = response.json()
+		user_balance = user_data['balance']
+		print "		Your current wallet balance is:" + price_to_str(user_balance)
+			
+
 def process_rfid(rfid):
 	global entry,cart_id
 	if(str(rfid) == '553128273391'):
@@ -41,8 +84,10 @@ def process_rfid(rfid):
 			cart_id = str(cart_data['id'])
 		else:
 			print "Exit"
+			show_bill_summary(user_id)
 			response = requests.post(url = CART_RESOURCE, data = cart_req_js, headers=headers)
 			print response.status_code
+			entry = True
 	else:
 		if(len(cart_id) == 0):
 			print "User not logged in"
@@ -54,6 +99,18 @@ def process_rfid(rfid):
 			cart_uri = CART_RESOURCE + cart_id
 			print cart_uri
 			response = requests.post(url = cart_uri, data = cart_item_req_js, headers=headers)
+			if('itemId' in response.text):
+				cart_item_data = response.json()
+				cart_item_name = cart_item_data['item']['name']
+				cart_item_price = int(cart_item_data['item']['price'])
+				cart_item_quantity = cart_item_data['quantity']
+				item_price_full = str(cart_item_price/100)
+				item_price_dec = str(cart_item_price%100)
+				if(len(item_price_dec) == 1):
+					item_price_dec+='0'
+				print "Item "+ cart_item_name + " added to your cart, Quantity: " + str(cart_item_quantity) + " Price: " + item_price_full + "." + item_price_dec
+			else:
+				print "Item removed"
 			print(response.status_code)
 			
 
@@ -63,7 +120,7 @@ try:
 	while 1==1:
         	id, text = reader.read()
 		process_rfid(id)
-		time.sleep(5)
+		time.sleep(3)
 finally:
         GPIO.cleanup()
 
